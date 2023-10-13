b2ea9e58621e42cde9239a13472cd3e448308e6d
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-10-13 10:07:30 -0400

    Access table
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- nodes/list.go
+++ nodes/list.go
@@ -1,6 +1,8 @@
 package nodes
 
 import (
+	"log"
+
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -13,8 +15,8 @@ func (c *Client) List() (map[string]*Node, error) {
 
 	if err := c.ls.DoString(`local N = require("node_library")
 local nodes = {}
-for k, v in pairs(N:listNodes()) do
-    table.insert(nodes, v)
+for _, v in pairs(N:listNodes()) do
+    nodes[v] = N:getNode(v)
 end
 return nodes
 `); err != nil {
@@ -23,11 +25,15 @@ return nodes
 
 	lv := c.ls.Get(-1) // get the value at the top of the stack
 	if tbl, ok := lv.(*lua.LTable); ok {
-		// fmt.Println(c.ls.ObjLen(tbl))
-		tbl.ForEach(func(_, v lua.LValue) {
-			result[v.String()] = &Node{}
+		tbl.ForEach(func(k, v lua.LValue) {
+			log.Printf("Node['%v'] = %#v", k, v)
+			result[k.String()] = &Node{}
 		})
 	}
 
 	return result, nil
 }
+
+/*
+2023/10/13 10:06:48 Node['Helix'] = &lua.LTable{Metatable:(*lua.LNilType)(0x102877b80), array:[]lua.LValue(nil), dict:map[lua.LValue]lua.LValue(nil), strdict:map[string]lua.LValue{"inputs":(*lua.LTable)(0x14000488c60), "label":"Helix", "op":(*lua.LFunction)(0x1400048abc0), "outputs":(*lua.LTable)(0x14000489080), "returns":"out_mesh"}, keys:[]lua.LValue{"label", "op", "inputs", "outputs", "returns"}, k2i:map[lua.LValue]int{"inputs":2, "label":0, "op":1, "outputs":3, "returns":4}}
+*/
