598c0c90e25ab2d3927afc2ede3e65c31457285b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-05 21:21:52 -0500

    Change STL dump algorithm
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- nodes/stl.go
+++ nodes/stl.go
@@ -34,20 +34,71 @@ func (c *Client) ToSTL(design *ast.BJK, filename string) error {
 	return out.Close()
 }
 
-func tesselateFace(out *stl.Client, mesh *Mesh, faceIndex int) error {
+type stlWriter interface {
+	Write(t *stl.Tri) error
+}
+
+func (v Vec3) tof32arr() [3]float32 {
+	return [3]float32{float32(v.X), float32(v.Y), float32(v.Z)}
+}
+
+func tesselateFace(out stlWriter, mesh *Mesh, faceIndex int) error {
 	face := mesh.Faces[faceIndex]
 	if len(face) < 3 {
 		return fmt.Errorf("face <3 verts: %+v", face)
 	}
+
 	faceNormal := mesh.CalcFaceNormal(faceIndex)
-	n := [3]float32{float32(faceNormal.X), float32(faceNormal.Y), float32(faceNormal.Z)}
-	pt1 := mesh.Verts[face[0]]
-	v1 := [3]float32{float32(pt1.X), float32(pt1.Y), float32(pt1.Z)}
-	for i := 2; i < len(face); i++ {
-		pt2, pt3 := mesh.Verts[face[i-1]], mesh.Verts[face[i]]
-		v2 := [3]float32{float32(pt2.X), float32(pt2.Y), float32(pt2.Z)}
-		v3 := [3]float32{float32(pt3.X), float32(pt3.Y), float32(pt3.Z)}
-		t := &stl.Tri{N: n, V1: v1, V2: v2, V3: v3}
+	n := faceNormal.tof32arr()
+	// v1 := mesh.Verts[face[0]].tof32arr()
+
+	// for i := 2; i < len(face); i++ {
+	// 	v2, v3 := mesh.Verts[face[i-1]].tof32arr(), mesh.Verts[face[i]].tof32arr()
+	// 	t := &stl.Tri{N: n, V1: v1, V2: v2, V3: v3}
+	// 	if err := out.Write(t); err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	// Note that this function is sent convex and concave polygons.
+	// The algorithms needed to handle these are a pain, so this function
+	// just uses a simple heuristic to start at one vertex, and make a triangle
+	// with the vertices on either side of it, then works its way on alternating
+	// sides of the face to the other end.
+	numVerts := len(face)
+	lastCWIdx, lastCCWIdx := 0, 1
+	for {
+		v1 := mesh.Verts[face[lastCWIdx]].tof32arr()
+		v2 := mesh.Verts[face[lastCCWIdx]].tof32arr()
+
+		// advance CW
+		lastCWIdx = (lastCWIdx - 1 + numVerts) % numVerts
+		if lastCWIdx == lastCCWIdx {
+			break
+		}
+		v3 := mesh.Verts[face[lastCWIdx]].tof32arr()
+		t := &stl.Tri{
+			N:  n,
+			V1: v1,
+			V2: v2,
+			V3: v3,
+		}
+		if err := out.Write(t); err != nil {
+			return err
+		}
+
+		// advance CCW
+		lastCCWIdx = (lastCCWIdx + 1) % numVerts
+		if lastCWIdx == lastCCWIdx {
+			break
+		}
+		v4 := mesh.Verts[face[lastCCWIdx]].tof32arr()
+		t = &stl.Tri{
+			N:  n,
+			V1: v2,
+			V2: v4,
+			V3: v3,
+		}
 		if err := out.Write(t); err != nil {
 			return err
 		}
---
+++ nodes/stl_test.go
@@ -0,0 +1,70 @@
+package nodes
+
+import (
+	"testing"
+
+	"github.com/gmlewis/irmf-slicer/v3/stl"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestTesselateFace(t *testing.T) {
+	mesh := &Mesh{
+		Verts: []Vec3{
+			{11.50, -0.50, 0.00},
+			{10.20, -0.50, 5.31},
+			{6.59, -0.50, 9.42},
+			{7.10, -0.50, 10.29},
+			{11.07, -0.50, 5.81},
+			{12.50, -0.50, 0.00},
+		},
+		Faces: [][]int{
+			{0, 1, 2, 3, 4, 5},
+		},
+	}
+
+	out := &fakeSTLWriter{}
+	if err := tesselateFace(out, mesh, 0); err != nil {
+		t.Fatal(err)
+	}
+
+	got := out.tris
+	if diff := cmp.Diff(wantTris, got); diff != "" {
+		t.Errorf("tesselateFace mismatch (-want +got):\n%v", diff)
+	}
+}
+
+type fakeSTLWriter struct {
+	tris []*stl.Tri
+}
+
+func (f *fakeSTLWriter) Write(t *stl.Tri) error {
+	f.tris = append(f.tris, t)
+	return nil
+}
+
+var wantTris = []*stl.Tri{
+	{
+		N:  [3]float32{0, 1, 0},
+		V1: [3]float32{11.5, -0.5, 0},
+		V2: [3]float32{10.199999809265137, -0.5, 5.309999942779541},
+		V3: [3]float32{12.5, -0.5, 0},
+	},
+	{
+		N:  [3]float32{0, 1, 0},
+		V1: [3]float32{10.199999809265137, -0.5, 5.309999942779541},
+		V2: [3]float32{6.590000152587891, -0.5, 9.420000076293945},
+		V3: [3]float32{12.5, -0.5, 0},
+	},
+	{
+		N:  [3]float32{0, 1, 0},
+		V1: [3]float32{12.5, -0.5, 0},
+		V2: [3]float32{6.590000152587891, -0.5, 9.420000076293945},
+		V3: [3]float32{11.069999694824219, -0.5, 5.809999942779541},
+	},
+	{
+		N:  [3]float32{0, 1, 0},
+		V1: [3]float32{6.590000152587891, -0.5, 9.420000076293945},
+		V2: [3]float32{7.099999904632568, -0.5, 10.289999961853027},
+		V3: [3]float32{11.069999694824219, -0.5, 5.809999942779541},
+	},
+}
