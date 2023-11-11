41b55ff7db89c3bc1c6e9693b2869a4c766140a1
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-11 12:13:33 -0500

    Sort non-manifold halfEdges by length
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- nodes/decimation.go
+++ nodes/decimation.go
@@ -3,6 +3,7 @@ package nodes
 import (
 	"fmt"
 	"log"
+	"sort"
 
 	"golang.org/x/exp/maps"
 )
@@ -33,6 +34,10 @@ func (fi *faceInfoT) decimatePhase1(vertIdx int) {
 		log.Printf("faceNormal[%v]=%v", faceIdx, fi.faceNormals[faceIdx])
 	}
 
+	// fi.m.Faces[5] = nil  // DEBUGGING ONLY!!!
+	// fi.m.Faces[10] = nil // DEBUGGING ONLY!!!
+	// fi.m.Faces[9] = nil  // DEBUGGING ONLY!!!
+
 	// step 1 - find all edge unit vectors (and their ending vertex indices)
 	// with respect to this vertex.
 	for {
@@ -48,15 +53,17 @@ func (fi *faceInfoT) decimatePhase1(vertIdx int) {
 type halfEdgeT struct {
 	edgeUnitNormal Vec3
 	toVertIdx      int
+	length         float64
 	onFaceIdx      int
 }
 
 func (h *halfEdgeT) String() string {
-	return fmt.Sprintf("{n: %v, toVertIdx: %v, onFaceIdx: %v}", h.edgeUnitNormal, h.toVertIdx, h.onFaceIdx)
+	return fmt.Sprintf("{n: %v, toVertIdx: %v, length=%v, onFaceIdx: %v}", h.edgeUnitNormal, h.toVertIdx, h.length, h.onFaceIdx)
 }
 
 // nextNonManifoldVerts finds the next collection of halfEdges that
-// are non-manifold and returns them, or nil if none are found.
+// are non-manifold and returns them (sorted by edge length descending),
+// or nil if none are found.
 func (fi *faceInfoT) nextNonManifoldVerts(vertIdx int) []*halfEdgeT {
 	seenNormals := map[string]map[int]*halfEdgeT{}
 	var resultKey string
@@ -73,7 +80,10 @@ func (fi *faceInfoT) nextNonManifoldVerts(vertIdx int) []*halfEdgeT {
 		}
 	}
 
-	return maps.Values(seenNormals[resultKey])
+	result := maps.Values(seenNormals[resultKey])
+	sort.Slice(result, func(i, j int) bool { return result[i].length > result[j].length })
+
+	return result
 }
 
 // getHalfEdges returns both halfEdges for the given vertIdx and face.
@@ -86,15 +96,19 @@ func (fi *faceInfoT) getHalfEdges(vertIdx, faceIdx int) []*halfEdgeT {
 		}
 		nextVertIdx := face[(i+1)%len(face)]
 		lastVertIdx := face[(i-1+len(face))%len(face)]
+		nextEdge := fi.m.Verts[nextVertIdx].Sub(v)
+		lastEdge := fi.m.Verts[lastVertIdx].Sub(v)
 		return []*halfEdgeT{
 			{
-				edgeUnitNormal: fi.m.Verts[nextVertIdx].Sub(v).Normalized(),
+				edgeUnitNormal: nextEdge.Normalized(),
 				toVertIdx:      nextVertIdx,
+				length:         nextEdge.Length(),
 				onFaceIdx:      faceIdx,
 			},
 			{
-				edgeUnitNormal: fi.m.Verts[lastVertIdx].Sub(v).Normalized(),
+				edgeUnitNormal: lastEdge.Normalized(),
 				toVertIdx:      lastVertIdx,
+				length:         lastEdge.Length(),
 				onFaceIdx:      faceIdx,
 			},
 		}
@@ -104,18 +118,34 @@ func (fi *faceInfoT) getHalfEdges(vertIdx, faceIdx int) []*halfEdgeT {
 
 /*
 decimatePhase1: vertIdx=2 {0.50 -0.50 4.50}, faceIdxes=[0 3 5 6 9 10]
-2023/11/11 08:51:58 face[0]={{-0.50 -0.50 3.50} {0.50 -0.50 3.50} {0.50 -0.50 4.50} {-0.50 -0.50 4.50}}
-2023/11/11 08:51:58 face[3]={{-0.50 -0.50 4.50} {0.50 -0.50 4.50} {0.50 1.50 4.50} {-0.50 1.50 4.50}}
-2023/11/11 08:51:58 face[5]={{0.50 1.50 4.50} {0.50 -0.50 4.50} {0.50 -0.50 3.50} {0.50 1.50 3.50}}
-2023/11/11 08:51:58 face[6]={{0.50 -0.50 3.50} {2.50 -0.50 3.50} {2.50 -0.50 4.50} {0.50 -0.50 4.50}}
-2023/11/11 08:51:58 face[9]={{0.50 -0.50 4.50} {2.50 -0.50 4.50} {2.50 0.50 4.50} {0.50 0.50 4.50}}
-2023/11/11 08:51:58 face[10]={{0.50 0.50 4.50} {0.50 0.50 3.50} {0.50 -0.50 3.50} {0.50 -0.50 4.50}}
-2023/11/11 08:51:58 faceNormal[0]={0.00000 -1.00000 0.00000}
-2023/11/11 08:51:58 faceNormal[3]={-0.00000 0.00000 1.00000}
-2023/11/11 08:51:58 faceNormal[5]={1.00000 0.00000 -0.00000}
-2023/11/11 08:51:58 faceNormal[6]={0.00000 -1.00000 0.00000}
-2023/11/11 08:51:58 faceNormal[9]={-0.00000 0.00000 1.00000}
-2023/11/11 08:51:58 faceNormal[10]={-1.00000 0.00000 0.00000}
+2023/11/11 11:47:32 face[0]={{-0.50 -0.50 3.50} {0.50 -0.50 3.50} {0.50 -0.50 4.50} {-0.50 -0.50 4.50}}
+2023/11/11 11:47:32 face[3]={{-0.50 -0.50 4.50} {0.50 -0.50 4.50} {0.50 1.50 4.50} {-0.50 1.50 4.50}}
+2023/11/11 11:47:32 face[5]={{0.50 1.50 4.50} {0.50 -0.50 4.50} {0.50 -0.50 3.50} {0.50 1.50 3.50}}
+2023/11/11 11:47:32 face[6]={{0.50 -0.50 3.50} {2.50 -0.50 3.50} {2.50 -0.50 4.50} {0.50 -0.50 4.50}}
+2023/11/11 11:47:32 face[9]={{0.50 -0.50 4.50} {2.50 -0.50 4.50} {2.50 0.50 4.50} {0.50 0.50 4.50}}
+2023/11/11 11:47:32 face[10]={{0.50 0.50 4.50} {0.50 0.50 3.50} {0.50 -0.50 3.50} {0.50 -0.50 4.50}}
+2023/11/11 11:47:32 faceNormal[0]={0.00000 -1.00000 0.00000}
+2023/11/11 11:47:32 faceNormal[3]={-0.00000 0.00000 1.00000}
+2023/11/11 11:47:32 faceNormal[5]={1.00000 0.00000 -0.00000}
+2023/11/11 11:47:32 faceNormal[6]={0.00000 -1.00000 0.00000}
+2023/11/11 11:47:32 faceNormal[9]={-0.00000 0.00000 1.00000}
+2023/11/11 11:47:32 faceNormal[10]={-1.00000 0.00000 0.00000}
+2023/11/11 12:11:28 fromVertIdx=2, nonManiVerts[0] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 6, length=2, onFaceIdx: 5}, (toVert={0.50000 1.50000 4.50000})
+2023/11/11 12:11:28 fromVertIdx=2, nonManiVerts[1] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 11, length=1, onFaceIdx: 10}, (toVert={0.50000 0.50000 4.50000})
+2023/11/11 11:47:32 nonManiVerts: got 2
+
+with faces[5]=nil:
+2023/11/11 11:56:55 fromVertIdx=2, nonManiVerts[0] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 6, length=2, onFaceIdx: 3}, (toVert={0.50000 1.50000 4.50000})
+2023/11/11 11:56:55 fromVertIdx=2, nonManiVerts[1] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 11, length=1, onFaceIdx: 10}, (toVert={0.50000 0.50000 4.50000})
+2023/11/11 11:56:55 nonManiVerts: got 2
+
+with faces[5]=nil and faces[10]=nil:
+2023/11/11 11:58:23 fromVertIdx=2, nonManiVerts[0] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 6, length=2, onFaceIdx: 3}, (toVert={0.50000 1.50000 4.50000})
+2023/11/11 11:58:23 fromVertIdx=2, nonManiVerts[1] = {n: {0.00000 1.00000 0.00000}, toVertIdx: 11, length=1, onFaceIdx: 9}, (toVert={0.50000 0.50000 4.50000})
+2023/11/11 11:58:23 nonManiVerts: got 2
+
+with faces[5]=nil and faces[10]=nil and faces[9]=nil:
+NONE FOUND - MAKES SENSE!!!
 */
 
 func (fi *faceInfoT) decimatePhase2(vertIdx int) {
