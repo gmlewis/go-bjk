9a446ad2ac520e7758d4c1fd3fde0977a466c44b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-17 21:36:31 -0500

    Unit tests pass
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- nodes/cut-neighbors.go
+++ nodes/cut-neighbors.go
@@ -1,17 +1,14 @@
 package nodes
 
 import (
-	"log"
 	"slices"
-
-	"golang.org/x/exp/maps"
 )
 
 func (is *infoSetT) cutNeighborsAndShortenFaceOnEdge(baseFaceIdx faceIndexT, move Vec3, edge edgeT, newCutFaceOKToAdd func(FaceT) bool) {
-	log.Printf("BEFORE cutNeighborsAndShortenFaceOnEdge(baseFaceIdx=%v, move=%v, edge=%v), #faces=%v\n%v", baseFaceIdx, move, edge, len(is.faces), is.faceInfo.m.dumpFaces(is.faces))
+	// log.Printf("BEFORE cutNeighborsAndShortenFaceOnEdge(baseFaceIdx=%v, move=%v, edge=%v), #faces=%v\n%v", baseFaceIdx, move, edge, len(is.faces), is.faceInfo.m.dumpFaces(is.faces))
 	baseFace := is.faces[baseFaceIdx]
 	oldVertsToNewMap := is.moveVerts(baseFace, move)
-	log.Printf("oldVertsToNewMap: %+v", oldVertsToNewMap)
+	// log.Printf("oldVertsToNewMap: %+v", oldVertsToNewMap)
 	affectedFaces := map[faceIndexT]bool{}
 
 	for vertIdx := range oldVertsToNewMap {
@@ -22,28 +19,24 @@ func (is *infoSetT) cutNeighborsAndShortenFaceOnEdge(baseFaceIdx faceIndexT, mov
 			affectedFaces[faceIdx] = true
 		}
 	}
-	log.Printf("cutNeighborsAndShortenFaceOnEdge found %v affected faces: %+v", len(affectedFaces), maps.Keys(affectedFaces))
-	verts := is.faceInfo.m.Verts
+	// log.Printf("cutNeighborsAndShortenFaceOnEdge found %v affected faces: %+v", len(affectedFaces), maps.Keys(affectedFaces))
+	// verts := is.faceInfo.m.Verts
 
 	for faceIdx := range affectedFaces {
 		face := is.faces[faceIdx]
 		originalFaceNormal := is.faceNormals[faceIdx]
 		oldCutFace := make(FaceT, 0, len(face))
 		newCutFace := make(FaceT, 0, len(face)/2)
-		var faceHasEdge bool
 		for i, vertIdx := range face {
 			if newIdx, ok := oldVertsToNewMap[vertIdx]; ok {
-				log.Printf("changing face[%v][%v] from vertIdx=%v=%v to vertIdx=%v=%v", faceIdx, i, vertIdx, verts[vertIdx], newIdx, verts[newIdx])
+				// log.Printf("changing face[%v][%v] from vertIdx=%v=%v to vertIdx=%v=%v", faceIdx, i, vertIdx, verts[vertIdx], newIdx, verts[newIdx])
 				is.faces[faceIdx][i] = newIdx
 				oldCutFace = append(oldCutFace, vertIdx)
 				newCutFace = append(newCutFace, newIdx)
 			}
-			nextIdx := face[(i+1)%len(face)]
-			if makeEdge(vertIdx, nextIdx) == edge {
-				faceHasEdge = true // leave this face shortened without filling in the gap.
-			}
 		}
-		if faceHasEdge {
+		if makeEdge(oldCutFace[0], oldCutFace[1]) == edge || makeEdge(newCutFace[0], newCutFace[1]) == edge {
+			// log.Printf("cutNeighborsAndShortenFaceOnEdge: will NOT create a face on this edge! oldCutFace=%v, newCutFace=%v", oldCutFace, newCutFace)
 			continue
 		}
 
@@ -57,19 +50,19 @@ func (is *infoSetT) cutNeighborsAndShortenFaceOnEdge(baseFaceIdx faceIndexT, mov
 				slices.Reverse(oldCutFace)
 				newFaceNormal = is.faceInfo.m.CalcFaceNormal(oldCutFace)
 				if !newFaceNormal.AboutEq(originalFaceNormal) {
-					log.Printf("WARNING: unable to make new face %+v normal (%v) same as original %+v (%v), skipping", oldCutFace, newFaceNormal, face, originalFaceNormal)
+					// log.Printf("WARNING: unable to make new face %+v normal (%v) same as original %+v (%v), skipping", oldCutFace, newFaceNormal, face, originalFaceNormal)
 					continue
 				}
 			}
 
-			log.Printf("adding new cut face: %+v", oldCutFace)
+			// log.Printf("adding new cut face: %+v", oldCutFace)
 			is.faces = append(is.faces, oldCutFace)
-		} else {
-			log.Printf("NOT ADDING new cut face: %+v !!!", oldCutFace)
+			//		} else {
+			//			log.Printf("NOT ADDING new cut face: %+v !!!", oldCutFace)
 		}
 	}
 
-	log.Printf("AFTER cutNeighborsAndShortenFaceOnEdge(baseFaceIdx=%v, move=%v, edge=%v), #faces=%v\n%v", baseFaceIdx, move, edge, len(is.faces), is.faceInfo.m.dumpFaces(is.faces))
+	// log.Printf("AFTER cutNeighborsAndShortenFaceOnEdge(baseFaceIdx=%v, move=%v, edge=%v), #faces=%v\n%v", baseFaceIdx, move, edge, len(is.faces), is.faceInfo.m.dumpFaces(is.faces))
 }
 
 // moveVerts creates new (or reuses old) vertices and returns the mapping from the
