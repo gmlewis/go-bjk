b18819d271b05ec876732dd3201136f41ed75ffa
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-03-20 21:50:05 -0400

    Update make-herringbone-gear
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- cmd/make-herringbone-gear/main.go
+++ cmd/make-herringbone-gear/main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 
 	"github.com/gmlewis/go-bjk/nodes"
@@ -15,10 +14,15 @@ import (
 
 var (
 	debug      = flag.Bool("debug", false, "Turn on debugging info")
-	helixAngle = flag.Float64("ha", 45, "Helix angle")
+	helixAngle = flag.Float64("ha", 30, "Helix angle")
+	holeRadius = flag.Float64("hr", 10, "Hole minimum radius")
+	holeType   = flag.String("ht", "None", "Hole type (one of: 'None', 'Hollow', 'Squared', 'Hexagonal', 'Octagonal', 'Circular')")
+	module     = flag.Float64("mod", 3, "Gear module - see https://www.stlgears.com/theory#module")
+	numTeeth   = flag.Int("nt", 13, "Number of teeth")
 	objOut     = flag.String("obj", "make-herringbone-gear.obj", "Output filename for Wavefront obj file")
 	outBJK     = flag.String("o", "make-herringbone-gear.bjk", "Output filename for BJK file ('-' for stdout, '' for none)")
 	repoDir    = flag.String("repo", "src/github.com/gmlewis/blackjack", "Path to Blackjack repo (relative to home dir or absolute path)")
+	resolution = flag.Int("res", 9, "Resolution for number of points in curved sections")
 	stlOut     = flag.String("stl", "make-herringbone-gear.stl", "Output filename for binary STL file")
 	swapYZ     = flag.Bool("swapyz", true, "Swap Y and Z values when writing STL file (Wavefront obj always swaps for Blender)")
 )
@@ -30,15 +34,15 @@ func main() {
 	must(err)
 	defer c.Close()
 
-	r := 22.5
-	for i := 0; i < 26; i++ {
-		ratio := float64(i) / 26
-		x := r * math.Cos(2*math.Pi*ratio)
-		y := r * math.Sin(2*math.Pi*ratio)
-		log.Printf("i=%v, angle=%.2f=%0.2f, (%.2f, %0.2f, 30)", i, 2*math.Pi*ratio, 360*ratio, x, y)
-	}
-
-	design, err := c.NewBuilder().AddNode("HerringboneGear", set("helix_angle", *helixAngle)).Build()
+	design, err := c.NewBuilder().AddNode(
+		"HerringboneGear",
+		set("helix_angle", *helixAngle),
+		set("hole_radius", *holeRadius),
+		set("hole_type", *holeType),
+		set("module", *module),
+		set("num_teeth", *numTeeth),
+		set("resolution", *resolution),
+	).Build()
 	must(err)
 
 	if *outBJK == "-" {
